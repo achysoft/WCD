310b773585a95a855aa7dee9454a51f56cb56990
Achim Nyabuto <achysofttechnologies@gmail.com>
2025-06-10 13:19:22 +0300

    added header to home

--- nwcd.amband.co.ke/nwcd/app/Views/frontend/home.php
+++ nwcd.amband.co.ke/nwcd/app/Views/frontend/home.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Home - Frontend</title>
-</head>
-<body>
-    <h1>Welcome to the Home Page</h1>
-    <p>This is your frontend home page.</p>
-    <a href="<?= site_url('about') ?>">About Us</a> |
-    <a href="<?= site_url('contact') ?>">Contact</a>
+<html lang="en" data-theme="light">
+
+<?= $this->include('./partials/head') ?>
+
+<body class="dark:bg-neutral-800 bg-neutral-100 dark:text-white">
+
+    <!-- ..::  header area start ::.. -->
+   <?= $this->include('./partials/navbar') ?>
+    <!-- ..::  header area end ::.. -->
+
+    
+
+    <!-- ..::  scripts  start ::.. -->
+    <?= $this->include('./partials/script') ?>
+    <!-- ..::  scripts  end ::.. -->
+
+    <?= $this->renderSection('./script') ?>
 </body>
+
 </html>
