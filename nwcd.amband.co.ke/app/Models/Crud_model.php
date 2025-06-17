21bd61a1e6a5e4e720e6417f7cacef90799967fd
Achim Nyabuto <achysofttechnologies@gmail.com>
2025-06-17 14:38:46 +0300

    changes to add to Reserve pins

--- nwcd.amband.co.ke/app/Models/Crud_model.php
+++ nwcd.amband.co.ke/app/Models/Crud_model.php
@@ -6,11 +6,24 @@ use CodeIgniter\Model;
 
 class Crud_model extends Model
 {
-    protected $table = 'parks'; // Important!
+    protected $table; // Set dynamically
     protected $primaryKey = 'id';
-
     protected $allowedFields = [
         'name', 'latitude', 'longitude', 'icon_url', 'location',
         'size_km', 'establishment', 'major_species', 'govenance_status', 'description'
     ];
+    protected $useTimestamps = false; // Enable if using created_at/updated_at
+    protected $returnType = 'array'; // 'object' if you prefer objects
+
+    /**
+     * Dynamically set the table name.
+     *
+     * @param string $tableName
+     * @return $this
+     */
+    public function setTable($tableName)
+    {
+        $this->table = $tableName;
+        return $this;
+    }
 }
